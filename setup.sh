cdf973dc672d0a5bf78a8611f02ec256a094bcf2
Eero Ruohola <eero@ruohola.dev>
2019-02-22 23:40:16 +0200

    setup.sh is working now

--- setup.sh
+++ setup.sh
@@ -1,36 +1,37 @@
 #!/usr/bin/env bash
 
-reponame=".dotfiles"
-current_dirname="$(cd "$(dirname "$0")" && basename $PWD)"
 
-if [ "$current_dirname" != "$reponame" ]; then
-    echo Renamed $current_dirname to $reponame
-    cd "$(dirname "$0")" && mv ../${current_dirname} ../${reponame}
+dotfiles=".dotfiles"
+
+# change the name of the folder with the git repo to $dotfiles
+current_dirname="$(cd "$(dirname "$0")" && basename "$PWD")"
+if [ "$current_dirname" != "$dotfiles" ]; then
+    cd "$(dirname "$0")" && mv ../"$current_dirname" ../"$dotfiles"
+    echo "Renamed $current_dirname/ to $dotfiles/"
 fi
 
-# makes the needed symlinks if they don't exist
-# these don't work yet
 
-# if [ ! .e ~/.vim/ ]; then
-#     ln -s ~/.dotfiles/vim ~/.vim/
-# fi
+# make the needed symlinks if they don't exist
+cd ~ # makes sure that the symlinks are shown as relative paths with ls -la
 
-# if [ ! .e ~/.bash_profile ]; then
-#     ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
-# fi
+if [ ! -e .vim/ ]; then
+    echo "Made symlink .vim -> $dotfiles/vim"
+    ln -s "$dotfiles"/vim .vim
+fi
 
-# if [ ! .e ~/.gitconfig ]; then
-#     ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-# fi
+if [ ! -e .ideavimrc ]; then
+    ln -s "$dotfiles"/vim/.ideavimrc .ideavimrc
+    echo "Made symlink .ideavimrc -> $dotfiles/vim/.ideavimrc"
+fi
 
-# if [ ! .e ~/.hushlogin ]; then
-#     ln -s ~/.dotfiles/.hushlogin ~/.hushlogin
-# fi
+for file in "$dotfiles"/misc/.[^.]*; do
+    file=$(basename "$file")
+    if [ ! -e "$file" ]; then
+        ln -s "$dotfiles"/misc/"$file" "$file"
+        echo "Made symlink $file -> $dotfiles/misc/$file"
+    fi
+done
 
-# if [ ! .e ~/.ideavimrc ]; then
-#     ln -s ~/.dotfiles/.ideavimrc ~/.ideavimrc
-# fi
 
-# if [ ! .e ~/.inputrc ]; then
-#     ln -s ~/.dotfiles/.inputrc ~/.inputrc
-# fi
+# install all vim plugins (cannot be done in background)
+vim -c "PlugClean!" -c "PlugInstall" -c "qa!"
