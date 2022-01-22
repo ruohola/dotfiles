569ee9c29e3d19171c250d3d7a4b0199eb5c72b9
Eero Ruohola <eero@ruohola.dev>
2022-01-22 11:36:47 +0200

    Improve punctuation in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cd ~ || exit  # makes sure that the symlinks are shown as relative to ~ with ls -la
+cd ~ || exit  # Makes sure that the symlinks are shown as relative to `~` with `ls -la`.
 
-# make the needed symlinks if they don't exist
+# Make the needed symlinks if they don't exist.
 [ ! -L .vim ] && rm -rf .vim && ln -sv dotfiles/vim .vim
 
 [ ! -L .ideavimrc ] && ln -sfv dotfiles/vim/.ideavimrc .ideavimrc
@@ -19,20 +19,20 @@ for file in .gitconfig .gitignore_global; do
     [ ! -L "$file" ] && ln -sfv dotfiles/git/"$file" "$file"
 done
 
-# install homebrew and all brew packages
+# Install homebrew and all brew packages.
 [ ! -f /usr/local/bin/brew ] \
     && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" \
     && brew tap homebrew/bundle \
     && { brew bundle install --file=~/dotfiles/brew/Brewfile;
         /usr/local/opt/fzf/install; }
 
-# install vim-plug and all vim plugins
+# Install vim-plug and all vim plugins.
 [ ! -f ~/.vim/autoload/plug.vim ] \
     && curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \
     && vim -c "PlugClean!" -c "PlugInstall" -c "qa!"
 
-# the rest are on purpose as absolute links and not relative from ~
+# The rest are on purpose as absolute links and not relative from `~`.
 
 [ ! -L '/Library/Keyboard Layouts/Finner.keylayout' ] \
     && sudo ln -sfv ~/dotfiles/keylayouts/finner/Finner.keylayout '/Library/Keyboard Layouts/Finner.keylayout'
